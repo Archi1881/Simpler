f30feb63b946b4685bd1b3dbf686e04eb997b064
Archi1881 <detr1881@gmail.com>
2022-07-05 23:04:41 +0500

    lesson_20

---
+++ lib/simpler/controller.rb
@@ -0,0 +1,65 @@
+require_relative 'view'
+
+
+module Simpler
+  class Controller
+
+    attr_reader :name, :request, :response
+    attr_accessor :headers
+
+    def initialize(env)
+      @name = extract_name
+      @request = Rack::Request.new(env)
+      @response = Rack::Response.new
+      @headers  = @response.headers
+    end
+
+    def make_response(action)
+      @request.env['simpler.controller'] = self
+      @request.env['simpler.action'] = action
+
+      set_default_headers
+      send(action)
+      write_response
+
+      @response.finish
+    end
+    
+    private
+
+    def status(value)
+      @response.status = value
+    end
+
+    def extract_name
+      self.class.name.match('(?<name>.+)Controller')[:name].downcase
+    end
+
+    def set_default_headers
+      headers['Content-Type'] = plain_exists? ? 'text/plain' : 'text/html'
+    end
+
+    def plain_exists?
+      template = @request.env['simpler.template']
+
+      types = %i[plain inline text body]
+
+      template.is_a?(Hash) && template.any? { |type| types.include?(type) }
+    end
+
+    def write_response
+      body = render_body
+
+      @response.write(body)
+    end
+
+    def render_body
+      View.new(@request.env).render(binding)
+    end
+
+    def render(template)
+      @request.env['simpler.template'] = template
+    end
+
+  end
+end
---
+++ lib/simpler/router/route.rb
@@ -0,0 +1,44 @@
+module Simpler
+  class Router
+    class Route
+
+      attr_reader :controller, :action
+
+      def initialize(method, path, controller, action)
+        @method = method
+        @path = path
+        @controller = controller
+        @action = action
+      end
+
+      def match?(method, path)
+        @method == method && path.match(@path)
+      end
+
+      def params(env)
+        request = Rack::Request.new(env)
+      end
+
+      private
+
+      def make_params(env_info)
+        path = extract_params(@path)
+        requests = extract_params(env_info)
+        result = {}
+
+        path.zip(requests) do |key, value|
+          key = key.delete(':').to_sym
+          result[key] = value
+        end
+        result
+      end
+
+      def extract_params(path_split)
+        path_split = path_split.split('/')
+        path_split.delete_at(0)
+        path_split
+      end
+    
+    end
+  end
+end
